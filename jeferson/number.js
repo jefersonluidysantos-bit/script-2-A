63588f920004d60e06979cd1e393a057b3398753
jefersonluidysantos-bit <jeferson.luidy.santos@escola.pr.gov.br>
2026-03-12 08:31:48 -0300

    Add files via upload

---
+++ jeferson/number.js
@@ -0,0 +1,4 @@
+const numero = 3.3;
+const numero2 = .5;
+const calculo = numero / numero2;
+console.log(calculo);
---
+++ jeferson/string.js
@@ -0,0 +1,9 @@
+//exemplos de texto
+let nome = "jeferson";
+let sobreNome = "luidy";
+let nomCompleto = nome + sobreNome;
+//concatenação das variaveis
+console.log(nomCompleto)
+let senha = "senha1234";
+let stringDenumeros = "123456789";
+console.log(stringDenumeros + 2);
