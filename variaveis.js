a0a398853806f98713013179d92fde8d902b1ecd
jefersonluidysantos-bit <jeferson.luidy.santos@escola.pr.gov.br>
2026-03-18 10:05:12 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,14 @@
+var altura = 5;
+var comprimento = 7;
+area = altura * comprimento;
+let area
+let forma = "retângulo";
+if(forma === "retângulo"){
+    area = altura * comprimento;
+}else{
+    area = (altura * comprimento)/2;
+}
+
+
+console.log(area)
+
